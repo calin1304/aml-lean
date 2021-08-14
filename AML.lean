5f3bb513f091239314ba2ed25a7bfcda3545d5f6
Calin Nicolau <calin.nicolau@gmail.com>
2021-08-14 09:59:04 +0300

    wip-junk

--- AML.lean
+++ AML.lean
@@ -1,6 +1,11 @@
 namespace AML
+  -- structure signature
+  --   (evar : Type)
+  --   (svar : Type)
+  --   (symbols : Type)
+
   -- Encoding variables with De Brujin indices
-  structure evar := mk :: (dbi : ℕ) /-^ Element variables-/
+  structure evar := mk :: (dbi : ℕ) /-^ Element variables -/
   structure svar := mk :: (dbi : ℕ) /-^ Set variables -/
 
   structure box := mk /-^ Box placeholder for application contexts -/
@@ -13,153 +18,150 @@ namespace AML
     | svar : svar -> var_type
     | box : var_type
 
-  /-- AML formulas, which we call patterns because of the pattern matching semantics.
-    -/
+  /-- AML formulas, which we call patterns because of the pattern matching semantics. -/
   inductive pattern : Type
-    | var   : var_type -> pattern 
-    | evar : evar -> pattern
-    | svar : svar -> pattern
+    | evar  : evar -> pattern
+    | svar  : svar -> pattern
     | sym   : string -> pattern
     | app   : pattern -> pattern -> pattern
     | bot   : pattern
     | impl  : pattern -> pattern -> pattern
     | exist : evar -> pattern -> pattern
-    | mu    : svar -> pattern -> pattern
+    | mu    : svar -> pattern -> pattern -- TODO: Positivity check
 
-  open pattern 
+  notation `⊥` := pattern.bot
+  notation φ `->` ψ := pattern.impl φ ψ
+  notation `∃` x `:` φ := pattern.exist x φ
 
-  notation `⊥` := bot
-  notation p `->` q := impl p q
-  notation `∃` x `:` p := exist x p
+  def not (φ : pattern) : pattern := φ -> ⊥
+  notation `¬`:40 φ := not φ
 
-  def not (p : pattern) : pattern := p -> bot
-  notation `¬` p := not p
-
-  def or (p q : pattern) : pattern := ¬ p -> q
-  notation p `∨` q := or p q
+  def or (φ ψ : pattern) : pattern := ¬φ -> ψ
+  notation φ `∨`:30 ψ := or φ ψ
   
-  def and (p q : pattern) : pattern := ¬(¬p ∨ ¬q)
-  notation p `∧` q := and p q
+  def and (φ ψ : pattern) : pattern := ¬(¬φ ∨ ¬ψ)
+  notation φ `∧`:35 ψ := and φ ψ
   
-  def top : pattern := not bot
+  def top : pattern := not ⊥
   notation `⊤` := top
   
-  def equiv (p q : pattern) : pattern := (p -> q) ∧ (q -> p)
-  notation p `<->` q := equiv p q
+  def equiv (φ ψ : pattern) : pattern := (φ -> ψ) ∧ (ψ -> φ)
+  notation φ `<->`:20 ψ := equiv φ ψ
 
-  def all (x : evar) (p : pattern) : pattern := ¬ (∃x: ¬ p)
-  notation `.∀` x `:` p := all x p
-
-  def nu (X : svar) (p : pattern) : pattern := sorry
-
-  namespace AML.instances
-    instance evar_to_pattern : has_coe evar pattern := ⟨λ v, var (var_type.evar v)⟩
-    instance svar_to_pattern : has_coe svar pattern := ⟨λ v, var (var_type.svar v)⟩
-
-    instance evar_to_var_type : has_coe evar var_type := ⟨var_type.evar⟩
-    instance svar_to_var_type : has_coe svar var_type := ⟨var_type.svar⟩
-
-    instance var_type_to_pattern : has_coe var_type pattern := ⟨var⟩
-
-    instance evar.decidable_eq : decidable_eq evar := 
-      λ ⟨s₁⟩ ⟨s₂⟩,
-          match string.has_decidable_eq s₁ s₂ with
-            | is_true p := is_true (congr_arg evar.mk p)
-            | is_false p := is_false (λ q, p (evar.mk.inj q))
-          end
-    instance svar.decidable_eq : decidable_eq svar := 
-      λ ⟨s₁⟩ ⟨s₂⟩,
-          match string.has_decidable_eq s₁ s₂ with
-            | is_true p := is_true (congr_arg svar.mk p)
-            | is_false p := is_false (λ q, p (svar.mk.inj q))
-          end
-
-    instance var_type.decidable_eq : decidable_eq var_type
-      | (var_type.evar v₁) (var_type.evar v₂) := 
-          match evar.decidable_eq v₁ v₂ with
-            | is_true p := is_true (congr_arg var_type.evar p)
-            | is_false p := is_false (λ q, p (var_type.evar.inj q))
-          end
-      | (var_type.svar v₁) (var_type.svar v₂) :=
-          match svar.decidable_eq v₁ v₂ with
-            | is_true p := is_true (congr_arg var_type.svar p)
-            | is_false p := is_false (λ q, p (var_type.svar.inj q))
-          end
-      | var_type.box var_type.box := is_true rfl
-      | _ _ := is_false (λ h, sorry)
-  end AML.instances
-  
-  /-- The set of all free variables in a pattern -/
-  def pattern.FV : pattern -> set var_type
-    | (var v) := {v}
-    | (sym _) := ∅
-    | (app s arg) := s.FV ∪ arg.FV
-    | bot := ∅
-    | (impl p q) := p.FV ∪ q.FV
-    | (exist x p) := p.FV \ {x}
-    | (mu X p) := p.FV \ {X}
-
-  /-- Substitute variable `x` with pattern `y` in given pattern.
-    - TODO: Fix this
-    -/
-  def pattern.subst (y : pattern) (x : var_type) : pattern -> pattern
-    | (var v) := if v = x then y else var x
-    | (sym s) := sym s
-    | (app s arg) := app (pattern.subst s) (pattern.subst arg)
-    | bot := bot
-    | (impl p q) := impl (pattern.subst p) (pattern.subst q)
-    | (exist z p) := 
-        if ↑z = x
-          then exist z p
-          else if ↑z ∈ y.FV then sorry else exist z (pattern.subst p)
-    | (mu X p) := sorry
-
-  notation p `[` y `//` x `]` := pattern.subst y x p
-
-  section
-    example :
-      let p := (sym "+"), x : evar := ⟨"x"⟩, y : evar := ⟨"y"⟩
-      in p[x <- y] = p := rfl
-    example :
-      let p : evar := ⟨"x"⟩, x : evar := ⟨"x"⟩, y : evar := ⟨"y"⟩
-      in p[x <- y] = y := rfl
-  end
-
-  def pattern.hasBound (x : var_type) (p : pattern) : Prop := ¬ (x ∈ p.FV)
+  def all (x : evar) (φ : pattern) : pattern := ¬(∃x: ¬φ)
+  notation `.∀` x `:` φ := all x φ
 
   /-- This should apply to μX.φ where X has to be positive in φ. It should not be nested
     - an odd number of times on the left of an implication φ₁ -> φ₂.
     -/
-  def positive (X : svar) (φ : pattern) : Prop := sorry
+  def isPositive (φ : pattern) (X : svar) : Prop := sorry
+
+  namespace Syntax.Substitution
+    /-- Substitution of set variables. -/
+    def pattern.ssubst (ψ : pattern) (X : svar) : pattern -> pattern := sorry
+
+    notation φ `[` ψ `///` X `]` := pattern.ssubst φ X φ
+
+    /-- Substitute variable `x` with pattern `y` in given pattern.
+    - TODO: Fix this
+    -/
+    def pattern.esubst (ψ : pattern) (x : evar) : pattern -> pattern := sorry
+    -- | (var v) := if v = x then y else var x
+    -- | (sym s) := sym s
+    -- | (app s arg) := app (pattern.subst s) (pattern.subst arg)
+    -- | bot := bot
+    -- | (impl p q) := impl (pattern.subst p) (pattern.subst q)
+    -- | (exist z p) := 
+    --     if ↑z = x
+    --       then exist z p
+    --       else if ↑z ∈ y.FV then sorry else exist z (pattern.subst p)
+    -- | (mu X p) := sorry
+
+    notation p `[` y `//` x `]` := pattern.esubst y x p
+  end Syntax.Substitution
+
+  namespace AML.instances
+    open AML
+
+    instance evar_to_pattern : has_coe evar pattern := ⟨λ v, pattern.evar v⟩
+    instance svar_to_pattern : has_coe svar pattern := ⟨λ v, pattern.svar v⟩
+
+    instance evar_to_var_type : has_coe evar var_type := ⟨var_type.evar⟩
+    instance svar_to_var_type : has_coe svar var_type := ⟨var_type.svar⟩
+
+    -- instance var_type_to_pattern : has_coe var_type pattern := ⟨var⟩
+
+    -- instance evar.decidable_eq : decidable_eq evar := 
+    --   λ ⟨s₁⟩ ⟨s₂⟩,
+    --       match string.has_decidable_eq s₁ s₂ with
+    --         | is_true p := is_true (congr_arg evar.mk p)
+    --         | is_false p := is_false (λ q, p (evar.mk.inj q))
+    --       end
+    -- instance svar.decidable_eq : decidable_eq svar := 
+    --   λ ⟨s₁⟩ ⟨s₂⟩,
+    --       match string.has_decidable_eq s₁ s₂ with
+    --         | is_true p := is_true (congr_arg svar.mk p)
+    --         | is_false p := is_false (λ q, p (svar.mk.inj q))
+    --       end
+
+    -- instance var_type.decidable_eq : decidable_eq var_type
+    --   | (var_type.evar v₁) (var_type.evar v₂) := 
+    --       match evar.decidable_eq v₁ v₂ with
+    --         | is_true p := is_true (congr_arg var_type.evar p)
+    --         | is_false p := is_false (λ q, p (var_type.evar.inj q))
+    --       end
+    --   | (var_type.svar v₁) (var_type.svar v₂) :=
+    --       match svar.decidable_eq v₁ v₂ with
+    --         | is_true p := is_true (congr_arg var_type.svar p)
+    --         | is_false p := is_false (λ q, p (var_type.svar.inj q))
+    --       end
+    --   | var_type.box var_type.box := is_true rfl
+    --   | _ _ := is_false (λ h, sorry)
+  end AML.instances
+
+  def nu (X : svar) (φ : pattern) (h : isPositive φ X) : pattern :=
+    ¬(pattern.mu X (¬(φ[¬X /// X])))
+  
+  /-- The set of all free variables in a pattern -/
+  def pattern.FV : pattern -> set var_type := sorry
+    -- | (var v) := {v}
+    -- | (sym _) := ∅
+    -- | (app s arg) := s.FV ∪ arg.FV
+    -- | bot := ∅
+    -- | (impl p q) := p.FV ∪ q.FV
+    -- | (exist x p) := p.FV \ {x}
+    -- | (mu X p) := p.FV \ {X}
+
+  def pattern.hasBound (x : var_type) (p : pattern) : Prop := sorry -- ¬ (x ∈ p.FV)
 end AML
 
 namespace AML.AppCtx
   open AML
   open AML.pattern
 
-  def isAppCtx : pattern -> Prop
-    | (var v) := if v = var_type.box then true else false
-    | (sym s) := false
-    | (app f arg) := sorry
-    | bot := false
-    | (impl p q) := false
-    | (exist x p) := false
-    | (mu X p) := false
+  def isAppCtx : pattern -> Prop := sorry
+    -- | (var v) := if v = var_type.box then true else false
+    -- | (sym s) := false
+    -- | (app f arg) := sorry
+    -- | bot := false
+    -- | (impl p q) := false
+    -- | (exist x p) := false
+    -- | (mu X p) := false
 
   structure appCtx := appCtx ::
     (p : pattern)
     (prop_isAppCtx : isAppCtx p)
 
-  def appCtx.subst (C : appCtx) (p : pattern) : pattern :=
-    match C.p with
-      | (var v) := sorry
-      | (sym s) := sorry
-      | (app f arg) := sorry
-      | bot := sorry
-      | (impl p q) := sorry
-      | (exist x p) := sorry
-      | (mu X p) := sorry
-    end
+  def appCtx.subst (C : appCtx) (p : pattern) : pattern := sorry
+    -- match C.p with
+    --   | (var v) := sorry
+    --   | (sym s) := sorry
+    --   | (app f arg) := sorry
+    --   | bot := sorry
+    --   | (impl p q) := sorry
+    --   | (exist x p) := sorry
+    --   | (mu X p) := sorry
+    -- end
 
   notation C `[` p `]` := C.subst p
 
@@ -173,33 +175,32 @@ namespace AML.Proof
   open AML.pattern
   open AML.AppCtx
 
-  variables {p q r : pattern}
-  variables { x y : evar }
+  variables {φ ψ ξ : pattern}
+  variables {x y : evar}
+  variable {X : svar}
   variables {C C₁ C₂ : appCtx}
 
-  inductive thm : pattern -> Type*
+  inductive thm : pattern -> Type
     -- Propositional logic
-    | a1 : thm (p -> (q -> p))
-    | a2 : thm ((p -> (q -> r)) -> ((p -> q) -> (p -> r)))
-    | a3 : thm ((¬p -> ¬q) -> (q -> p))
-    | mp : thm p -> thm (p -> q) -> thm q
+    | a1 : thm (φ -> (ψ -> φ))
+    | a2 : thm ((φ -> (ψ -> ξ)) -> ((φ -> ψ) -> (φ -> ξ)))
+    | a3 : thm ((¬φ -> ¬φ) -> (ψ -> φ))
+    | mp : thm φ -> thm (φ -> ψ) -> thm ψ
     -- FOL
-    | ex_quan : thm (p[y//x] -> (∃x:p))
-    | ex_gen : q.hasBound x -> thm (p -> q) -> thm (∃x:p -> q)
+    | ex_quan : thm (φ[y//x] -> (∃x:φ))
+    | ex_gen : ψ.hasBound x -> thm (φ -> ψ) -> thm (∃x:φ -> ψ)
     -- Frame reasoning
     | propg_bot : thm (C[⊥] -> ⊥)
-    | propg_disj : thm (C[p ∨ q] -> C[p] ∨ C[q])
-    | propg_exist : C.hasBound x -> thm (C[∃x: p] -> ∃x : (C[p]))
-    | framing : thm (p -> q) -> thm (C[p] -> C[q])
-    -- Technical rules
+    | propg_disj : thm (C[φ ∨ ψ] -> C[φ] ∨ C[ψ])
+    | propg_exist : C.hasBound x -> thm (C[∃x: φ] -> ∃x : (C[φ]))
+    | framing : thm (φ -> ψ) -> thm (C[φ] -> C[ψ])
+    -- Fixpoint reasoning
+    | svar_subst : thm φ -> thm (φ[ψ///X])
+    | pre_fixpoint : thm (φ[mu X φ///X] -> mu X φ)
+    | knaster_tarski : thm (φ[ψ///X] -> ψ) -> thm (mu X φ -> ψ)
+    -- -- Technical rules
     | existence : thm (∃x:x)
-    | singleton : nestedAppCtx C₁ -> nestedAppCtx C₂ -> thm ¬(C₁[x ∧ p] ∧ C₂[x ∧ ¬ p])
-    -- TODO: set variable subst
-    -- TODO: pre-fixpoint
-    -- TODO: knaster-tarksi
-
-  notation `⊢` p := thm p
-
+    | singleton : nestedAppCtx C₁ -> nestedAppCtx C₂ -> thm ¬(C₁[x ∧ φ] ∧ C₂[x ∧ ¬ φ])
 end AML.Proof
 
 namespace AML.Semantics
